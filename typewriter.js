df4bc95b11a0ddac675005611a584767edb17993
Nikolai Jensen <124652004+NikJensen97@users.noreply.github.com>
2023-03-14 19:48:05 -0700

    Delete typewriter.js
    oops!

--- typewriter.js
+++
@@ -1,9 +0,0 @@
-const sentence = `hello there from lighthouse labs\n`;
-let timeout = 50;
-  for (const char of sentence) {
-    setTimeout(() => {
-      process.stdout.write(char);
-    }, timeout);
-    // adds 50 extra MS to each character so they dont print at the same time
-    timeout += 50;
-  }
